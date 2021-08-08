ecde0cfe13641e607f25a61399d27681e8a310f1
Damiano Magrini <damiano.magrini@gmail.com>
2021-08-08 08:52:59 +0200

    chore: update example

--- example/src/screens/ScreenB.tsx
+++ example/src/screens/ScreenB.tsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { RoutingFunctionsContext } from 'react-stack-navigator';
 
 export const ScreenB: React.FC = () => {
-	const { pop } = useContext(RoutingFunctionsContext);
+	const { push, pop } = useContext(RoutingFunctionsContext);
 
 	return (
 		<main className='screen-container'>
 			<h1>Screen B</h1>
 			<button onClick={() => pop(Math.random())}>Pop!</button>
+			<button onClick={async () => push(<ScreenB />)}>Push!</button>
 		</main>
 	);
 };
